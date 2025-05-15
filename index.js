eddeff91d4b339143b9c09196e488fe646855987
Rajath <rajatha363@gmail.com>
2025-05-15 21:25:00 +0530

    Initial commit has only the base working website

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import qr from "qr-image";
+import bodyParser from "body-parser"
+
+const app = express();
+const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs");
+});
+app.post("/submit",(req,res)=>{
+    var urlLink = req.body.URL;
+    var qr_png = qr.imageSync( urlLink, { type: 'png' });
+    const imageBase64 = qr_png.toString('base64');
+    const imageSrc = `data:image/png;base64,${imageBase64}`;
+    res.render("index.ejs", {
+        imageSrc: imageSrc, 
+    });
+});
+
+app.listen(port,()=>{
+    console.log(`server atarted at ${port}`);
+});
